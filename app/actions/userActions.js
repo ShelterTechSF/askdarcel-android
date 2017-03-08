8d2cd158a4aa66608ca4b1809ef2b1761da249a1
Silverhawk <sondra.silverhawk@hush.com>
2017-03-08 15:15:09 -0800

    Add actions for user login

--- app/actions/userActions.js
+++ app/actions/userActions.js
@@ -2,8 +2,18 @@ import {
   GET_USER_LOCATION,
   GET_USER_LOCATION_FULFILLED,
   GET_USER_LOCATION_ERROR,
-  UPDATE_USER_LOCATION
+  UPDATE_USER_LOCATION,
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  LOGIN_REQUEST
 } from './actionTypes';
+import {
+  AsyncStorage,
+  Alert
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+import { API_URL } from '../config';
 
 export function getUserLocation() {
   return function(dispatch) {
@@ -31,3 +41,53 @@ export function updateUserLocation(location) {
     payload: location
   };
 };
+
+async function setStorage(item, selectedValue) {
+  try {
+    await AsyncStorage.setItem(item, selectedValue);
+  } catch (error) {
+    console.log('AsyncStorage error: ' + error.message);
+  }
+}
+
+export function userLogin(email, password)  {
+  return dispatch => {
+    dispatch({type: LOGIN_REQUEST});
+
+    fetch(API_URL + '/admin/auth/sign_in', {
+      method: 'post',
+      headers: {
+        "Content-type": "application/json", 
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    })
+    .then(response => {
+      if( response.status === 200) {
+        dispatch({type: LOGIN_SUCCESS, payload: {email}});
+        const headers = response.headers;
+        this.setStorage('userToken', headers.get('access-token'));
+        // localStorage.setItem('authHeaders', JSON.stringify({
+        //   "access-token":headers.get('access-token'),
+        //   client: headers.get('client'),
+        //   uid: headers.get('uid')
+        // }));
+      } else if( response.status === 401) {
+        Alert.alert(
+          'Problem with Login', 
+          'Your email or password was incorrect', 
+          [ {text: 'Continue Without Login', onPress: () => Actions.main()},  
+            {text: 'Try Again', onPress: () => Actions.login()}, 
+          ],
+           { cancelable: false }
+        );
+        dispatch({type: LOGIN_ERROR, payload: 'Incorrect email or password.'});
+      }
+    })
+    .catch(error => {
+      console.log('err', error);
+    })
+  }
+}
