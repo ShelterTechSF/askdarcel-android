5e207d8d5a56a0f61295bc87f91612dbf7df8eca
Silverhawk <sondra.silverhawk@hush.com>
2017-03-13 17:31:17 -0700

    Add launchbar to resource detail, launch website

--- app/components/LaunchBar.js
+++ app/components/LaunchBar.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { LinkLauncher } from './shared';
 
 class LinkBar extends Component {
   render() {
+    const { resource } = this.props;
     return (
       <View>
+        <LinkLauncher
+          url={resource.website} 
+          displayText={"website"}
+        />
       </View>
     );
   }
--- app/components/shared/LinkLauncher.js
+++ app/components/shared/LinkLauncher.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import {
   Linking,
+  Image,
   Text,
   TouchableOpacity,
   View
 } from 'react-native';
 
 class LinkLauncher extends Component {
+  onPress() {
+    Linking.canOpenURL(this.props.url).then(supported => { 
+      if (supported) { 
+        Linking.openURL(this.props.url); 
+      } else {
+       console.log('Don\'t know how to open URI: ' + this.props.url); 
+      } 
+    });
+  } 
+
   render() {
-    return <View />;
+    return (
+      <TouchableOpacity onPress={this.onPress.bind(this)}>
+        <View>
+          {this.props.imageSource && <Image source={this.props.imageSource} />}
+          {this.props.displayText && <Text>{this.props.displayText}</Text>}
+        </View>
+      </TouchableOpacity>
+    );
   }
 }
 
